62b7862a119cf989d9b490732350a5b67632e7ec
JohanCasserfelt <johan.casserfelt@gmail.com>
2019-10-25 15:16:12 +0200

    Working on makeAMove. now printing iKlick, jKlick, i0, k0. problem is there

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -82,43 +82,53 @@ public class GameFrame extends JFrame implements ActionListener {
         for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
                 if (board[i][j].getText().equals("0")) {
+
                     i0 = i;
                     j0 = j;
                     JButton blanc = board[i][j];
                 }
                 if (board[i][j].getText().equals(b.getText())) ;
+               // board[i][j].getText().equals(b.getText())
                 {
                     iKlick = i;
                     jKlick = j;
                     JButton tempB = b;
                 }
-                // to the left to the left
-                if ((iKlick == i0) && (jKlick == j0 - 1)) {
-                    tempString = board[i0][j0].getText();
-                    board[i0][j0].setText(board[iKlick][jKlick].getText());
-                    board[iKlick][jKlick].setText(tempString);
-                }
-                // to the right to the right
-                else if ((iKlick == i0) && (jKlick == j0 + 1)) {
-                    tempString = board[i0][j0].getText();
-                    board[i0][j0].setText(board[iKlick][jKlick].getText());
-                    board[iKlick][jKlick].setText(tempString);
-                }
-                // up
-                else if ((iKlick == i0 + 1) && (jKlick == j0)) {
-                    tempString = board[i0][j0].getText();
-                    board[i0][j0].setText(board[iKlick][jKlick].getText());
-                    board[iKlick][jKlick].setText(tempString);
-                }
-                // down
-                else if ((iKlick == i0 - 1) && (jKlick == j0)) {
-                    tempString = board[i0][j0].getText();
-                    board[i0][j0].setText(board[iKlick][jKlick].getText());
-                    board[iKlick][jKlick].setText(tempString);
-                }
-                swapwithNeibour();
             }
         }
+        /*// total misär
+        if(b.getText() == ){
+
+        }*/
+        // to the left to the left
+        if ((iKlick == i0) && (jKlick == (j0 - 1))) {
+            tempString = board[i0][j0].getText();
+            board[i0][j0].setText(board[iKlick][jKlick].getText());
+            board[iKlick][jKlick].setText(tempString);
+            swapwithNeibour();
+        }
+        // to the right to the right
+        else if ((iKlick == i0) && (jKlick == (j0 + 1))) {
+            tempString = board[i0][j0].getText();
+            board[i0][j0].setText(board[iKlick][jKlick].getText());
+            board[iKlick][jKlick].setText(tempString);
+            swapwithNeibour();
+        }
+        // up
+        else if ((iKlick == (i0 + 1)) && (jKlick == j0)) {
+            tempString = board[i0][j0].getText();
+            board[i0][j0].setText(board[iKlick][jKlick].getText());
+            board[iKlick][jKlick].setText(tempString);
+            swapwithNeibour();
+        }
+        // down
+        else if ((iKlick == (i0 - 1)) && (jKlick == j0)) {
+
+            tempString = board[i0][j0].getText();
+            board[i0][j0].setText(board[iKlick][jKlick].getText());
+            board[iKlick][jKlick].setText(tempString);
+            swapwithNeibour();
+        } else System.out.printf(" iKlick:\t%d\n jKlick:\t%d\n i0:\t\t%d\n j0:\t\t%d", iKlick, jKlick, i0, j0);
     }
 
     private void swapwithNeibour() {
