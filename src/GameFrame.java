2a971aabd47bb9e69290889cc94d0f92dbf80a3c
MarioSamperi75 <mariosss@libero.it>
2019-10-26 11:07:39 +0200

    new structure: start from TestClass och new class Move

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -1,7 +1,5 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
@@ -11,17 +9,14 @@ public class GameFrame extends JFrame /*implements ActionListener*/ {
 
     int SIZE = 4;
     int SQUARESIZE = SIZE * SIZE;
-    int i0 = 0;
+    /*int i0 = 0;
     int j0 = 0;
     int iKlick = 0;
-    int jKlick = 0;
+    int jKlick = 0;*/
     private JButton[][] board = new JButton[SIZE][SIZE];
     private JPanel panel = new JPanel();
 
-    public static void main(String[] args) {
-        new GameFrame();
-    }
-
+//
     public GameFrame() {
         initiateBoard();
         manageLayout();
@@ -100,83 +95,16 @@ public class GameFrame extends JFrame /*implements ActionListener*/ {
         public void mouseClicked(MouseEvent e) {
             super.mouseClicked(e);
             JButton tempButton = (JButton) e.getComponent();
-            makeAMove(tempButton);
+            Move move = new Move(tempButton, board, SIZE);
+           // makeAMove(tempButton);
             //System.out.println(tempButton.getText());
         }
     };
 
-    private void makeAMove(JButton b) {
-        String tempString;
-        for (int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE; j++) {
-                if (board[i][j].getText().equals("0")){
-                    i0 = i;
-                    j0 = j;
-                    //JButton tempB = b;
-                }
-                if (board[i][j].getText().equals(b.getText())) {
-                    iKlick = i;
-                    jKlick = j;
-                    JButton tempB = b;
-                }
-            }
-        }
-        // to the left to the left
-        if ((iKlick == i0) && (jKlick == (j0 - 1))) {
-            tempString = board[i0][j0].getText();
-            board[i0][j0].setText(board[iKlick][jKlick].getText());
-            board[iKlick][jKlick].setText(tempString);
-            swapwithNeibour();
-        }
-        // to the right to the right
-        else if ((iKlick == i0) && (jKlick == (j0 + 1))) {
-            tempString = board[i0][j0].getText();
-            board[i0][j0].setText(board[iKlick][jKlick].getText());
-            board[iKlick][jKlick].setText(tempString);
-            swapwithNeibour();
-        }
-        // up
-        else if ((iKlick == (i0 + 1)) && (jKlick == j0)) {
-            tempString = board[i0][j0].getText();
-            board[i0][j0].setText(board[iKlick][jKlick].getText());
-            board[iKlick][jKlick].setText(tempString);
-            swapwithNeibour();
-        }
-        // down
-        else if ((iKlick == (i0 - 1)) && (jKlick == j0)) {
-            tempString = board[i0][j0].getText();
-            board[i0][j0].setText(board[iKlick][jKlick].getText());
-            board[iKlick][jKlick].setText(tempString);
-            swapwithNeibour();
-        } else {
-            //System.out.printf(" iKlick:\t%d\n jKlick:\t%d\n i0:\t\t%d\n j0:\t\t%d", iKlick, jKlick, i0, j0);
-        }
-    }
 
-    private void swapwithNeibour() {
-        /*String tempString = "";
-        for (int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE; j++) {
-                if (board[i][j].getText().equals(b.getText())) {
-                    tempString = board[i][j - 1].getText();
-                    board[i][j - 1].setText(b.getText());
-                    b.setText(tempString);
-                    // this does not work
-                    if (board[i][j - 1].getText().equals("0")) {
-                        b.setVisible(false);
-                        board[i][j - 1].setVisible(true);
-                    }
-                }
-            }
-        }
-        */
-        for (int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE; j++) {
-                if (board[i][j].getText().equals("0")) {
-                    board[i][j].setVisible(false);
-                } else board[i][j].setVisible(true);
-            }
-        }
-    }
 
 }
+
+
+
+
